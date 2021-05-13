9f5a749076748387347b9703f83ca56be0001edf
Cuong Nguyen <cuong.hsgs@gmail.com>
2021-05-13 14:55:33 -0500

    first ver

---
+++ src/App.js
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/Resume";
+import Contact from "./components/Contact/Contact";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-tsparticles";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
+  const [load, upadateLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      upadateLoad(false);
+    }, 800);
+  }, []);
+
+  return (
+    <Router>
+      <Preloader load={load} />{" "}
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <Particles
+          options={{
+            backgroundMode: {
+              enable: true,
+              zIndex: 0,
+            },
+            particles: {
+              number: {
+                value: 10,
+                limit: 200,
+                density: {
+                  enable: true,
+                  value_area: 200,
+                },
+              },
+              color: {
+                value: "#ffffff",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 4,
+                },
+              },
+              opacity: {
+                value: 0.5,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.5,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 3,
+                  size_min: 3,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 1,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 3,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                  parallax: {
+                    enable: false,
+                    force: 20,
+                    smooth: 10,
+                  },
+                },
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 800,
+                  lineLinked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 800,
+                  size: 2,
+                  duration: 2,
+                  opacity: 1,
+                  speed: 2,
+                },
+                push: {
+                  particles_nb: 5,
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+            fps_limit: 144,
+          }}
+        />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />{" "}
+          <Route path="/project" component={Projects} />{" "}
+          <Route path="/about" component={About} />{" "}
+          <Route path="/resume" component={Resume} />{" "}
+          <Route path="/contact" component={Contact} />{" "}
+        </Switch>{" "}
+        <Footer />
+      </div>{" "}
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,84 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Cuong Nguyen </span> and
+            I am a{" "}
+            <span className="purple">
+              Senior Computer Science + Math Major{" "}
+            </span>
+            at <span className="purple">Texas Christian University</span>. I'm
+            an aspiring Software Engineering and IT Consultant.
+            <br />
+            <br />
+            Area of interest: Machine Learning, Algorithm, Mobile App, Data
+            Science.
+            <br />
+            <br />
+            My Honors and Awards:
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Awarded Bronze Medal in ACM ICPC South Central
+              USA Regional Contest (2019) - ranked 1st/19 total Undergraduate
+              school teams, ranked 12th/61 total teams, Language: C, C++)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Awarded 1st price in Hackathon UTA MLH chatbot
+              challenge (2018)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Ranked 27th/71 total teams in ACM ICPC South
+              Central USA Regional Contest(2018, Language: C, C++)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Upsilon Pi Epsilon (2019), TCU ACM Team Leader
+              (2018+2019)
+            </li>
+          </ul>
+          <p style={{ textAlign: "justify" }}>My Skills/ Expertise:</p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Awarded Bronze Medal in ACM ICPC South Central
+              USA Regional Contest (2019) - ranked 1st/19 total Undergraduate
+              school teams, ranked 12th/61 total teams, Language: C, C++)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Language: C, C++, Python, Java, JavaScript, VBS,
+              VBA, Pascal, HTML, CSS, SQL, Lisp, Scala
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Software/APIs: OpenCV, Git/Github, Aircrack,
+              Node.js, Mysql, Latex
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Platforms: Window, Mac, Linux (Ubuntu, Fedora,
+              Linux Mint, CentOS)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Coursework: Data Structures, Algorithms,
+              Databases, Wireless Networks and Security, High Performance and
+              Parallel Computing, Artificial Intelligence
+            </li>
+          </ul>
+          <br/>
+          <p style={{ textAlign: "justify" }}>
+            StrengthsQuest: Adaptable, Flexible, Competitive, and Open-To-Change
+          </p>
+          <br/>
+          <p style={{ textAlign: "justify" }}>
+            Hobbies: video games, reading historical stories, swimming, and playing board
+            games.</p>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
