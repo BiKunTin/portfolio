c6abe8c5b1a149258434c0c5102dacfe00742cfc
Cuong Nguyen <cuong.hsgs@gmail.com>
2021-05-21 13:35:14 -0500

    update name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Cuong Nguyen</strong>
+                <strong className="main-name"> Kelvin Nguyen</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
